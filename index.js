af7d3276362e997fa85f4197a1d1c704604f2e59
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2022-11-22 15:45:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+
+
 // Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names,event) {
+  let messages = []
+  for (let i = 0; i < names.length; i++) {
+    const message= `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    messages.push(message)
+  }
+
+  return messages;
+}
+
+
+
+const countDown= function (n){
+  while(n>=0){
+    console.log(n);
+    n--
+  }
+}
+ 
+
